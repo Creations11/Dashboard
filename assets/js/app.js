dfa4ee2e65ae8fc757d5d3e593507036d1eabf2d
Easybranding <sogulaa@gmail.com>
2026-05-11 15:02:34 +0200

    Update app.js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,2 +1,17 @@
-window.onload
-initializeDashboard()
+window.onload = async () => {
+
+    loadTheme();
+
+    runComplianceChecks();
+
+    await loadApplicationsFromCloud();
+
+    setInterval(async () => {
+
+        await loadApplicationsFromCloud();
+
+    }, AUTO_REFRESH_INTERVAL);
+
+    showToast("5Clipa Cloud Platform Ready ✔️");
+
+};

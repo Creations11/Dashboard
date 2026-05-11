cca0731a57778092fcbd7a1a005f6535459fc63c
Easybranding <sogulaa@gmail.com>
2026-05-11 14:58:01 +0200

    Update constants.js

--- assets/js/constants.js
+++ assets/js/constants.js
@@ -1,3 +1,6 @@
-const API_URL = "...";
-const API_KEY = "...";
+const API_URL =
+"https://script.google.com/macros/s/AKfycbw_BVBoWr2VNi0gJkgkqShQXFTLugmd-3qN4ORyWKz-tu7CoUkXOcmtsnEALMvaYaU6AA/exec";
+
+const API_KEY = "EE_SECURE_2026_KEY";
+
 const AUTO_REFRESH_INTERVAL = 30000;

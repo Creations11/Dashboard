127bda15be7617ad052d42fd52d89b23d8170adc
Easybranding <sogulaa@gmail.com>
2026-05-13 11:46:14 +0200

    Add exportFullReport function to generate PDF report

--- assets/js/pdf.js
+++ assets/js/pdf.js
@@ -1,2 +1,33 @@
-generateReceipt()
-exportApplications()
+function exportFullReport(){
+
+const element =
+document.body;
+
+const options = {
+
+margin:0.5,
+filename:"5clipa-report.pdf",
+
+image:{
+type:"jpeg",
+quality:1
+},
+
+html2canvas:{
+scale:2
+},
+
+jsPDF:{
+unit:"in",
+format:"a4",
+orientation:"portrait"
+}
+
+};
+
+html2pdf()
+.set(options)
+.from(element)
+.save();
+
+}

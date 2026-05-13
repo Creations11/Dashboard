eb8268fef417a6b7cbbb77b8c6afa44cdf0fb31a
Easybranding <sogulaa@gmail.com>
2026-05-13 11:57:05 +0200

    Simplify PDF report generation function
    Refactor PDF export function to simplify options and update filename.

--- assets/js/pdf.js
+++ assets/js/pdf.js
@@ -1,33 +1,11 @@
 function exportFullReport(){
 
-const element =
-document.body;
-
-const options = {
-
-margin:0.5,
-filename:"5clipa-report.pdf",
-
-image:{
-type:"jpeg",
-quality:1
-},
-
-html2canvas:{
-scale:2
-},
-
-jsPDF:{
-unit:"in",
-format:"a4",
-orientation:"portrait"
-}
-
-};
+showToast(
+"Generating PDF Report..."
+);
 
 html2pdf()
-.set(options)
-.from(element)
-.save();
+.from(document.body)
+.save("5clipa-dashboard-report.pdf");
 
 }
